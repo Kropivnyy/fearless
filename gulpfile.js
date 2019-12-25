ab61a2beb727a0f577c3aba817a812d4aa82d459
Alexander Kropivnyy <kropivnyy@gmail.com>
2019-12-25 03:30:56 +0200

    merge 3 branches

--- gulpfile.js
+++ gulpfile.js
@@ -25,7 +25,7 @@ const watcher = done => {
     'change',
     series(tasks.html, tasks.inject, browserSync.reload),
   );
-  watch(paths.watch.css).on('change', series(tasks.css, browserSync.reload));
+  watch(paths.watch.css).on('change', function(){setTimeout( series(tasks.css, browserSync.reload), 100)});
   watch(paths.watch.js).on('change', series(tasks.scripts, browserSync.reload));
   watch(paths.watch.images, tasks.images);
   watch(paths.watch.fonts, tasks.fonts);
